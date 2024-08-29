10ed4ce63d1d3ce0b8a2beeca32cce4b690402af
amsurio <sim.amsurio@gmail.com>
2024-08-29 16:38:34 +0200

    init

---
+++ resources/js/Pages/Users/Create.vue
@@ -0,0 +1,67 @@
+<template>
+  <layout title="Create User">
+    <h1 class="mb-8 font-bold text-3xl">
+      <inertia-link class="text-indigo-light hover:text-indigo-dark" :href="route('users')">Users</inertia-link>
+      <span class="text-indigo-light font-medium">/</span> Create
+    </h1>
+    <div class="bg-white rounded shadow overflow-hidden max-w-lg">
+      <form @submit.prevent="submit">
+        <div class="p-8 -mr-6 -mb-8 flex flex-wrap">
+          <text-input v-model="form.first_name" :errors="errors.first_name" class="pr-6 pb-8 w-full lg:w-1/2" label="First name" />
+          <text-input v-model="form.last_name" :errors="errors.last_name" class="pr-6 pb-8 w-full lg:w-1/2" label="Last name" />
+          <text-input v-model="form.email" :errors="errors.email" class="pr-6 pb-8 w-full lg:w-1/2" label="Email" />
+          <text-input v-model="form.password" :errors="errors.password" class="pr-6 pb-8 w-full lg:w-1/2" type="password" autocomplete="new-password" label="Password" />
+          <select-input v-model="form.owner" :errors="errors.owner" class="pr-6 pb-8 w-full lg:w-1/2" label="Owner">
+            <option :value="true">Yes</option>
+            <option :value="false">No</option>
+          </select-input>
+        </div>
+        <div class="px-8 py-4 bg-grey-lightest border-t border-grey-lighter flex justify-end items-center">
+          <loading-button :loading="sending" class="btn-indigo" type="submit">Create User</loading-button>
+        </div>
+      </form>
+    </div>
+  </layout>
+</template>
+
+<script>
+import Layout from '@/Shared/Layout'
+import LoadingButton from '@/Shared/LoadingButton'
+import SelectInput from '@/Shared/SelectInput'
+import TextInput from '@/Shared/TextInput'
+
+export default {
+  components: {
+    Layout,
+    LoadingButton,
+    SelectInput,
+    TextInput,
+  },
+  props: {
+    errors: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  remember: 'form',
+  data() {
+    return {
+      sending: false,
+      form: {
+        first_name: null,
+        last_name: null,
+        email: null,
+        password: null,
+        owner: false,
+      },
+    }
+  },
+  methods: {
+    submit() {
+      this.sending = true
+      this.$inertia.post(this.route('users.store'), this.form)
+        .then(() => this.sending = false)
+    },
+  },
+}
+</script>
